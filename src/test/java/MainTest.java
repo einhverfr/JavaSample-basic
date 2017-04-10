76a016552f6ddbdc19846ac2877f92ce3240d11c
Chris Travers <chris.travers@gmail.com>
2017-04-10 12:38:42 +0200

    initial code

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Christopher Travers on 10/04/2017.
+ */
+public class MainTest {
+    @Test
+    public void minimal() throws Exception {
+        assertEquals(2, 1 + 1);
+    }
+
+}
